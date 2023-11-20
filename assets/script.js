52839cc413507f381d0975598e1474ce1781f481
Ruan Figuereido <huanfiguereido@gmail.com>
2023-11-20 18:39:18 -0300

    add: add a readme.md and fix:mobile menu are now responsible

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,41 @@
-let wid = window.screen.availWidth;
 let link = document.querySelectorAll("a");
 let menu = document.getElementById("links");
 let nav = document.querySelector("nav");
+let createHamburguer = false;
+window.addEventListener("resize", Menu);
+window.addEventListener("load", Menu);
 function Menu(){
-    if(wid<=900){
-//ATRIBUIÇÃO DA COR BRANCA AOS LINKS EM TELAS MENORES
-        for (let i = 0;i<5;i++){
-            link[i].style.color="#fff";
-        }
-//ESTILIZAÇÃO DO MENU DESLIZANTE
-        menu.style.position="absolute";
-        menu.style.top="100px";
-        menu.style.right="0";
-        menu.style.translate="100%";
-        menu.style.flexDirection="column";
-        menu.style.width="0%";
-        menu.style.height="90%";
-        menu.style.backgroundColor="rgb(0, 0, 19)";
-        menu.style.justifyContent="space-around";
-        menu.style.overflow="hidden";
 //CRIAÇÃO E APADRINHAMENTO DA IMG MENU HAMBURGUER
-        let imgMenu = document.createElement("img");
-        imgMenu.src="assets/images/icon-menu.svg";
-        nav.appendChild(imgMenu);
-        imgMenu.id="open";
-        let bg = document.getElementById("bg");
-        bg.src="assets/images/image-web-3-mobile.jpg";
-        let btn = document.querySelectorAll("nav img")[1];
-        btn.addEventListener("click", btnMenu);
-    }
+let wid = window.innerWidth;
+console.log(wid);
+        if(wid<=500 && createHamburguer == false){
+            let imgMenu = document.createElement("img");
+            imgMenu.src="assets/images/icon-menu.svg";
+            nav.appendChild(imgMenu);
+            imgMenu.id="open";
+            let bg = document.getElementById("bg");
+            bg.src="assets/images/image-web-3-mobile.jpg";
+            let btn = document.querySelectorAll("nav img")[1];
+            btn.addEventListener("click", btnMenu);
+            createHamburguer=true;
+        }
+        else if(wid>500 && createHamburguer==true){
+            document.querySelectorAll("nav img")[1].remove();
+        }
 }
 //FUNÇÃO ABRE E FECHA MENU
 function btnMenu(){
-    let buttonType = document.querySelectorAll("nav img")[1].id;
-    if(buttonType=="open"){
-        menu.style.width="50%";
-        menu.style.right="50%";
-        menu.style.transition="0.5s";
-        let buttonType = document.querySelectorAll("nav img")[1].id="close";
-        let imgMenu=document.getElementById("close");
-        imgMenu.src="assets/images/icon-menu-close.svg";
+    let buttonType = document.querySelectorAll("nav img")[1];
+    if(buttonType.id=="open"){
+        buttonType.src="assets/images/icon-menu-close.svg";
+        menu.style.right="65%";
+        menu.style.transition="0.3s";
+        buttonType.id="close";
     }
-     else if(buttonType=="close"){
-        menu.style.width="0%";
-        menu.style.right="0";
-        menu.style.translate="100%";
-        menu.style.transition="0.5s";
-        let buttonType = document.querySelectorAll("nav img")[1].id="open";
-        let imgMenu=document.getElementById("open");
-        imgMenu.src="assets/images/icon-menu.svg";
-     }
-    
+    else if(buttonType.id=="close"){
+        buttonType.src="assets/images/icon-menu.svg";
+        menu.style.right="0px";
+        menu.style.transition="0s";
+        buttonType.id="open";
+    }
 }
